e315cb99ba692465ad305f6bf7dc560a81ff1497
KILLER7sv <116873144+KILLER7sv@users.noreply.github.com>
2023-06-08 15:47:53 +0530

    solution added'

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -16,11 +16,8 @@ public class Main {
     public static void main(String[] args) {
         Product p = new Product();
         int first = p.product(5 , 6);
-        System.out.println(first);
         int second = p.product(5 , 6 ,2);
         double third = p.product(2,3);
-        System.out.println(second);
-        System.out.println(third);
     }
 
 }
